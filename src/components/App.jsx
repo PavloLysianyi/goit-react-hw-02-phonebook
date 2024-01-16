d9d25aa5f84dee0ac910ba753fbd10359a67d341
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2024-01-17 00:02:03 +0200

    do task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,182 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      number: '',
+    };
+  }
+
+  handleNameChange = event => {
+    this.setState({ name: event.target.value });
+  };
+
+  handleNumberChange = event => {
+    this.setState({ number: event.target.value });
+  };
+
+  handleAddContact = () => {
+    const { name, number } = this.state;
+    const { onAddContact, contacts } = this.props;
+
+    if (name.trim() === '' || number.trim() === '') {
+      alert("Будь ласка, введіть ім'я та номер контакту.");
+      return;
+    }
+
+    const isNameExists = contacts.some(contact => contact.name === name);
+
+    if (isNameExists) {
+      alert(`Контакт з ім'ям ${name} вже існує у телефонній книзі.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    onAddContact(newContact);
+    this.setState({
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <div className="form-container">
+        <label className="label">
+          Ім'я контакту:
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleNameChange}
+            required
+            className="input"
+          />
+        </label>
+        <label className="label">
+          Номер телефону:
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleNumberChange}
+            required
+            className="input"
+          />
+        </label>
+        <button
+          type="button"
+          onClick={this.handleAddContact}
+          className="button"
+        >
+          Додати контакт
+        </button>
+      </div>
+    );
+  }
+}
+
+class Filter extends React.Component {
+  render() {
+    const { filter, onFilterChange } = this.props;
+
+    return (
+      <input
+        type="text"
+        placeholder="Пошук за ім'ям"
+        value={filter}
+        onChange={onFilterChange}
+        className="input"
+      />
+    );
+  }
+}
+
+class ContactList extends React.Component {
+  render() {
+    const { contacts, onDeleteContact } = this.props;
+
+    return (
+      <ul className="list">
+        {contacts.map(contact => (
+          <li key={contact.id} className="list-item">
+            <span className="contact-info">
+              {contact.name} - {contact.number}
+            </span>
+            <button
+              type="button"
+              onClick={() => onDeleteContact(contact.id)}
+              className="delete-button"
+            >
+              Видалити
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ],
+      filter: '',
+    };
+  }
+
+  handleFilterChange = event => {
+    this.setState({ filter: event.target.value });
+  };
+
+  handleAddContact = newContact => {
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  handleDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div className="container">
+        <h1 className="heading">Phonebook</h1>
+        <ContactForm onAddContact={this.handleAddContact} contacts={contacts} />
+        <h2 className="sub-heading">Contacts</h2>
+        <Filter filter={filter} onFilterChange={this.handleFilterChange} />
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
